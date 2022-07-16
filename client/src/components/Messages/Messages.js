595619f77f379e1f31cf704a2499ea29cce2fa27
ydk1204 <ejrbdi@naver.com>
2022-07-16 21:10:33 +0900

    chat-app client and server

---
+++ client/src/components/Messages/Messages.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import ScollToBottom from 'react-scroll-to-bottom';
+import Message from '../Message/Message';
+
+import './Messages.css';
+
+const Messages = ({messages, name}) => (
+  <ScollToBottom className='messages'>
+    {messages.map((message, i) =>
+      <div key={i}>
+        <Message message={message} name={name} />
+      </div>
+    )}
+  </ScollToBottom>
+)
+
+export default Messages
