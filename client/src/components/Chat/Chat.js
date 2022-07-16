873e1843df273797af22224eff6a7f697d3357b2
ydk1204 <ejrbdi@naver.com>
2022-07-16 22:15:31 +0900

    server 배포 후 url 변경

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -15,7 +15,7 @@ const Chat = ({ location }) => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
 
-  const ENDPOINT = 'localhost:5000';
+  const ENDPOINT = `https://${process.env.REACT_APP_BACK_BASE_URL}`;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
